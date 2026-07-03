45d628568f0cc5bfb79ea5aa404048d4ff17de38
Sarah <duddaesarah@gmail.com>
2026-07-03 16:02:30 +0530

    lastcommit

--- frontend/index.js
+++ frontend/index.js
@@ -1,2 +1,28 @@
-console.log("Hello from js");
-alert("Hello from JS");
+const users = [
+  {
+    name: "Jane Doe",
+    gender: "Female",
+    image: "./images/jane.png"
+  },
+  {
+    name: "John Doe",
+    gender: "Male",
+    image: "./images/john.png"
+  }
+];
+
+let togId = 0;
+
+function toggle() {
+    if (togId === 0) {
+        togId = 1;
+    } else {
+        togId = 0;
+    }
+    //image
+    document.getElementById("im").src = users[togId].image;
+    //name
+    document.getElementById("name").textContent = users[togId].name;
+    //gender
+    document.getElementById("gender").textContent = users[togId].gender;
+}
