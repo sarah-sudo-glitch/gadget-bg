bbc0193ef76851bbc7347fc419c121e8f84b6ece
Sarah <duddaesarah@gmail.com>
2026-07-04 09:58:13 +0530

    first

--- frontend/index.js
+++ frontend/index.js
@@ -26,3 +26,21 @@ function toggle() {
     //gender
     document.getElementById("gender").textContent = users[togId].gender;
 }
+function getRandom(){
+  fetch("https://randomuser.me/api")
+  .then(function (data){
+    return data.json()
+  })
+  .then(function (parseData){
+    //console.log(parseData)
+    let first= parseData.results[0].name.first;
+    let last = parseData.results[0].name.last;
+    let name= first+" "+last;
+    let gender= parseData.results[0].gender;
+    let imageU= parseData.results[0].picture.large;
+
+    document.getElementById("gender").innerText=gender
+    document.getElementById("name").innerText=name
+    document.getElementById("im").src=imageU
+  })
+}
